f315d2a801bef2b0d245bab150ea52ce1e642b53
ki3ani <kimanim68@gmail.com>
2024-02-27 15:28:08 +0300

    mapping

--- cleanstore/cleanstore/settings.py
+++ cleanstore/cleanstore/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['cleanstoreobv-env.eba-pkj7kp8j.us-east-2.elasticbeanstalk.com','*']
