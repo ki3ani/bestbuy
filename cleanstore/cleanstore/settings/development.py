14c0faa8fde42841baab033e34445bb5aeb58775
ki3ani <kimanim68@gmail.com>
2024-03-06 10:28:24 +0300

    onli

--- cleanstore/cleanstore/settings/development.py
+++ cleanstore/cleanstore/settings/development.py
@@ -3,15 +3,14 @@ from .base import *
 
 DEBUG = True
 
-
-
+# Database configuration remains the same
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'staples',
-        'USER': 'ken',
-        'PASSWORD': 'invincible',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': 'cleanstore',
+        'USER': 'cleanstore_user',
+        'PASSWORD': 'ljBHs2QGlIjLwK1VVqQRtDajkVlXjnV9',
+        'HOST': 'dpg-cnk1g6821fec73e6phq0-a',  # Use the FQDN or IP address provided by Render
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
