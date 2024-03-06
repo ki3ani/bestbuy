2b609312a79b3d402a1865da5c774ec2e79620a7
ki3ani <kimanim68@gmail.com>
2024-03-06 23:27:37 +0300

    prius

--- cleanstore/cleanstore/settings/development.py
+++ cleanstore/cleanstore/settings/development.py
@@ -6,10 +6,10 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'staples',
-        'USER': 'ken',
-        'PASSWORD': 'invincible',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB', 'staples'),  
+        'USER': os.getenv('POSTGRES_USER', 'ken'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'invincible'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'), 
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
