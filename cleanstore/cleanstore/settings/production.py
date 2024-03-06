c78989b49a94a43b37b8fb148f24820b67e5382b
ki3ani <kimanim68@gmail.com>
2024-03-06 10:21:53 +0300

    onlinedb

--- cleanstore/cleanstore/settings/production.py
+++ cleanstore/cleanstore/settings/production.py
@@ -7,12 +7,10 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'staples',
-        'USER': 'ken',
-        'PASSWORD': 'invincible',
-        'HOST': 'db',  
-        'PORT': '5432',
+        'NAME': 'cleanstore',
+        'USER': 'cleanstore_user',
+        'PASSWORD': 'ljBHs2QGlIjLwK1VVqQRtDajkVlXjnV9',
+        'HOST': 'dpg-cnk1g6821fec73e6phq0-a',  # Use the FQDN or IP address provided by Render
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
-
